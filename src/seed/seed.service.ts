bb2a822c9966159d26b006ad49881eb955caf3f6
David Alexander Enamorado Aceituno <daenamorado@bancatlan.hn>
2022-11-16 22:57:53 -0600

    add: inyectar servicios a otro modulos

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { CarsService } from '../cars/cars.service';
+import { CARS_SEED } from './data/cars.seed';
+import { BrandsService } from '../brands/brands.service';
+import { BRANDS_SEED } from './data/brands.seed';
 
 @Injectable()
 export class SeedService {
+  constructor(
+    private readonly carsService: CarsService,
+    private readonly brandsService: BrandsService,
+  ) {}
   runSeed() {
+    this.carsService.fillCarsWithSeedDate(CARS_SEED);
+    this.brandsService.fillBrandWithSeedDate(BRANDS_SEED);
     return 'seed execute successfully';
   }
 }
